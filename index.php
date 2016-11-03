cfe60424c9264081020ad778a83d042d0713fc19
erwer8363 <1040908363@qq.com>
2016-11-04 01:09:10 +0800

    首页简单制作

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Insert title here</title>
+    <link rel="stylesheet" href="styles/1/basic.css">
+    <link rel="stylesheet" href="styles/1/index.css">
+</head>
+
+<body>
+    <div class="header">
+        <h1><a href="index.php">朴成web俱乐部多用户留言系统</a></h1>
+        <ul>
+            <li><a href="#">首页</a></li>
+            <li><a href="#">注册</a></li>
+            <li><a href="#">登录</a></li>
+            <li><a href="#">个人中心</a></li>
+            <li><a href="#">风格</a></li>
+            <li><a href="#">管理</a></li>
+            <li><a href="#">退出</a></li>
+        </ul>
+    </div>
+    <div class="list">
+        <h2>帖子列表</h2>
+    </div>
+    <div class="user">
+        <h2>新进会员</h2>
+    </div>
+    <div class="pics">
+        <h2>最新图片</h2>
+    </div>
+    <div class="footer">
+        <p>版权所有,翻版必究</p>
+        <p>本程序有<a href="#">朴成web俱乐部</a>所有,可以任意修改或发布(c)yc60.com</p>
+    </div>
+    <?php
+		
+	?>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+</body>
+
+</html>
